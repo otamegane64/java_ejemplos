bbcefe585304f854b1b849e11d16314f2a384436
otamegane2 <jesus6iribe4@gmail.com>
2023-07-30 10:40:12 -0700

    ejercicios 3 y 4
    se agregan ejercicios 3 y 4 de la plataforma hackerank

---
+++ src/ejemplo3.java
@@ -0,0 +1,22 @@
+/*import java.util.*;
+import java.io.*;
+import java.lang.Math;
+class Solution{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            int res = a;
+            for(int k=0;k<n;k++)
+            {
+                res += (int)(Math.pow(2, k) * b);
+                System.out.print(Integer.toString(res)+ " ");
+            }
+            System.out.print('\n');
+        }
+        in.close();
+    }
+}*/
