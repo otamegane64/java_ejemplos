adada53bd1580d37f7d373ebd08f42a19ee11d50
otamegane2 <jesus6iribe4@gmail.com>
2023-07-29 20:36:41 -0700

    se agregan ejemplos

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("================================");
+        for(int i=0;i<3;i++)
+        {
+            String s1=sc.next();
+            int x=sc.nextInt();
+            //Formatear caracteres
+            System.out.format("%-15s",s1);
+            //Formatear numeros
+            System.out.format("%03d%n", x);
+        }
+        System.out.println("================================");
+    }
+}
